4cc3064309b3683beb77a982e9cd98251d955fb6
Frizons <dmitriy.polunin.o@gmail.com>
2024-12-19 09:20:54 +0200

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.chrome.options import Options
-import time
 
 URL = "https://go.slotimo.com/login/"
 
